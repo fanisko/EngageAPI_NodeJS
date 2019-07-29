7f9d87eece9fc8f1cd97092b4045bc5a3e1aec61
prabhatm1432 <prabhatm@fanisko.com>
2019-07-29 17:38:48 +0530

    added routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,29 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var engage = require('fanisko-engage-api-test-prabhat')
+var engage = require('fanisko-engage-api')
 
-
+/**
+ * Getting Content
+ */
 router.post('/get_content',  async  function(req, res, next) {
   var content =  await engage.get_content(req.body["api_key"], req.body["secret_key"])
   res.status(200).json({ "result": content })
 });
 
+/**
+ * Getting Content with Id
+ */
 router.post('/get_content_by_id', async function(req, res, next) {
   var content = await engage.get_content_by_id(req.body["api_key"], req.body["secret_key"])
   res.status(200).json({ "result": content })
 });
-
-router.post('/post_content', async function(req, res, next) {
-  var xxx = await engage.post_content(req.body["api_key"], req.body["secret_key"],req.body['json'])
-  res.status(200).json({ "result": xxx })
-});
-
-router.post('/delete_content', async function(req, res, next) {
-  var xxx = await engage.delete_content(req.body["api_key"], req.body["secret_key"])
-  res.status(200).json({ "result": xxx })
-});
-
-
-
 
 module.exports = router;
