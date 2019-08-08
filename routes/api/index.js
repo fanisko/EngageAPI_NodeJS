3351f4a706bf39b845400d61121656c507330444
prabhatm1432 <prabhatm@fanisko.com>
2019-08-08 15:38:41 +0530

    added routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,20 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var engage = require('fanisko-engage-api')
+var engage = require('fanisko-engage-api');
+
+var API_KEY = "YOUR_API_KEY"
+var SECRET_KEY = "YOUR_SECRET_KEY"
+
 
 /**
  * Getting Content
  */
 router.post('/get_content',  async  function(req, res, next) {
-  var content =  await engage.get_content(req.body["api_key"], req.body["secret_key"])
+  var content =  await engage.get_content(API_KEY, SECRET_KEY)
   res.status(200).json({ "result": content })
 });
 
 /**
- * Getting Content with Id
+ * Getting Content by Id
  */
 router.post('/get_content_by_id', async function(req, res, next) {
-  var content = await engage.get_content_by_id(req.body["api_key"], req.body["secret_key"])
+  var content = await engage.get_content_by_id(API_KEY, SECRET_KEY)
+  res.status(200).json({ "result": content })
+});
+
+/**
+ * Getting Content by Pageno
+ */
+router.post('/get_content_by_page', async function(req, res, next) {
+  var content = await engage.get_content_by_id(API_KEY, SECRET_KEY,pageno)
   res.status(200).json({ "result": content })
 });
 
